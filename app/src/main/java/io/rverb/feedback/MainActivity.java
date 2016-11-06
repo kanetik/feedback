fdfc04709f0a0b2f7ec748c751752f2373933b96
Jeremy Kane <jkane001@gmail.com>
2016-11-06 17:56:41 -0500

    App for checking library implementation

---
+++ app/src/main/java/io/rverb/feedback/MainActivity.java
@@ -0,0 +1,29 @@
+package io.rverb.feedback;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import io.rverb.feedbacktest.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                HelpUtils.sendHelp(MainActivity.this, null);
+            }
+        });
+    }
+
+}
