854902d04d9428ad8eb55661ac8283a730942190
Jeremy Kane <jkane001@gmail.com>
2016-11-22 22:37:33 -0500

    Updated Session model

--- feedback/src/main/java/io/rverb/feedback/model/SessionData.java
+++ feedback/src/main/java/io/rverb/feedback/model/SessionData.java
@@ -4,22 +4,39 @@ import android.text.TextUtils;
 
 import java.io.Serializable;
 
+import io.rverb.feedback.utility.DateUtils;
+
 public class SessionData implements Serializable {
+    private static final long serialVersionUID = 348L;
+
+    public String appId;
     public String sessionId;
     public String supportId;
     public String userIdentifier;
+    public String sessionStartUtc;
 
-    public SessionData(String sessionId, String supportId) {
+    public SessionData(String appId, String sessionId, String supportId) {
+        this.appId = appId;
         this.sessionId = sessionId;
         this.supportId = supportId;
+        this.sessionStartUtc = DateUtils.nowUtc();
     }
 
-    public SessionData(String sessionId, String supportId, String userIdentifier) {
+    public SessionData(String appId, String sessionId, String supportId, String userIdentifier) {
+        this.appId = appId;
         this.sessionId = sessionId;
         this.supportId = supportId;
+        this.sessionStartUtc = DateUtils.nowUtc();
 
         if (!TextUtils.isEmpty(userIdentifier)) {
             this.userIdentifier = userIdentifier;
         }
+    }
+
+    @Override
+    public String toString() {
+        return "SessionId: " + sessionId + " | SupportId: " + supportId + " | UserIdentifier: "
+                + userIdentifier + " | SessionStartUTC" + sessionStartUtc;
+
     }
 }
