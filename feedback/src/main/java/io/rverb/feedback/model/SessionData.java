2811ec365a7201b92efc46cc2194d8b67e7073f0
Jeremy Kane <jkane001@gmail.com>
2016-11-25 11:58:00 -0500

    Removing UserIdentifier from SessionData

--- feedback/src/main/java/io/rverb/feedback/model/SessionData.java
+++ feedback/src/main/java/io/rverb/feedback/model/SessionData.java
@@ -1,7 +1,5 @@
 package io.rverb.feedback.model;
 
-import android.text.TextUtils;
-
 import java.io.Serializable;
 
 import io.rverb.feedback.utility.DateUtils;
@@ -22,21 +20,10 @@ public class SessionData implements Serializable {
         this.sessionStartUtc = DateUtils.nowUtc();
     }
 
-    public SessionData(String appId, String sessionId, String supportId, String userIdentifier) {
-        this.appId = appId;
-        this.sessionId = sessionId;
-        this.supportId = supportId;
-        this.sessionStartUtc = DateUtils.nowUtc();
-
-        if (!TextUtils.isEmpty(userIdentifier)) {
-            this.userIdentifier = userIdentifier;
-        }
-    }
-
     @Override
     public String toString() {
-        return "SessionId: " + sessionId + " | SupportId: " + supportId + " | UserIdentifier: "
-                + userIdentifier + " | SessionStartUTC" + sessionStartUtc;
+        return "SessionId: " + sessionId + " | SupportId: " + supportId
+                + " | SessionStartUTC" + sessionStartUtc;
 
     }
 }
