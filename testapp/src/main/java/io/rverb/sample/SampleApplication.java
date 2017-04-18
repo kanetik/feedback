afccace9cad594ffa648cfbeeab850279e977f03
Jeremy Kane <jkane001@gmail.com>
2017-04-17 21:54:43 -0400

    Sample Apps

--- testapp/src/main/java/io/rverb/sample/SampleApplication.java
+++ testapp/src/main/java/io/rverb/sample/SampleApplication.java
@@ -1,8 +1,17 @@
 package io.rverb.sample;
 
 import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.text.TextUtils;
+import android.util.Log;
 
 import com.facebook.stetho.Stetho;
+
+import java.util.Locale;
+import java.util.UUID;
 
 import io.rverb.feedback.Rverbio;
 
@@ -11,9 +20,24 @@ public class SampleApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
-        Stetho.initializeWithDefaults(this);
+        Stetho.initialize(Stetho.newInitializerBuilder(this)
+                .enableWebKitInspector(Stetho.defaultInspectorModulesProvider(this))
+                .build());
 
         Rverbio.initialize(this);
-        Rverbio.getInstance().addContextDataItem("Test 2", "This happened at Application#onCreate");
+        Rverbio.getInstance().setUserIdentifier(getSupportId(this));
+    }
+
+    public static String getSupportId(@NonNull Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        String supportId = prefs.getString("support_id", "");
+        if (TextUtils.isEmpty(supportId)) {
+            supportId = UUID.randomUUID().toString();
+            prefs.edit().putString("support_id", supportId).apply();
+        }
+
+        Log.d("EndUser", String.format(Locale.getDefault(), "%1$s: %2$s", "SupportId", supportId));
+
+        return supportId;
     }
 }
