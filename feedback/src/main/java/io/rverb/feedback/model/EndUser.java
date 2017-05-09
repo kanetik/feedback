a539a1d20a1b01ff25c8a33a0eff0ddb162b9e8b
Jeremy Kane <jkane001@gmail.com>
2017-05-08 21:50:56 -0400

    Keep track of sync state

--- feedback/src/main/java/io/rverb/feedback/model/EndUser.java
+++ feedback/src/main/java/io/rverb/feedback/model/EndUser.java
@@ -3,23 +3,27 @@ package io.rverb.feedback.model;
 import android.content.Context;
 import android.content.Intent;
 import android.os.ResultReceiver;
+import android.support.annotation.Keep;
 
 import java.util.UUID;
 
 import io.rverb.feedback.data.api.EndUserService;
 import io.rverb.feedback.utility.DataUtils;
 
+@Keep
 public class EndUser {
     public String endUserId;
     public String emailAddress;
     public String userIdentifier;
     public boolean isPersisted;
+    public boolean isSynced;
 
     public EndUser() {
         this.endUserId = UUID.randomUUID().toString();
         this.emailAddress = "";
         this.userIdentifier = "";
         this.isPersisted = false;
+        this.isSynced = false;
     }
 
     public Intent getPersistServiceIntent(Context context, ResultReceiver resultReceiver) {
