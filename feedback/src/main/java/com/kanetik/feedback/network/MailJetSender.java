91a6e40ff4b26361028e0338787e1760b554eb10
Jeremy Kane <jkane001@gmail.com>
2020-08-01 21:17:20 -0400

    no moshi

--- feedback/src/main/java/com/kanetik/feedback/network/MailJetSender.java
+++ feedback/src/main/java/com/kanetik/feedback/network/MailJetSender.java
@@ -6,7 +6,6 @@ import com.kanetik.feedback.model.Feedback;
 import com.mailjet.client.ClientOptions;
 import com.mailjet.client.MailjetClient;
 import com.mailjet.client.MailjetRequest;
-import com.mailjet.client.MailjetResponse;
 import com.mailjet.client.errors.MailjetException;
 import com.mailjet.client.errors.MailjetSocketTimeoutException;
 import com.mailjet.client.resource.Emailv31;
