b5df0d33258510b417b16a553b280cd5403dff2a
Jeremy Kane <jkane001@gmail.com>
2020-08-07 23:08:06 -0400

    Sending the right message

--- feedback/src/main/java/com/kanetik/feedback/network/MailJetSender.java
+++ feedback/src/main/java/com/kanetik/feedback/network/MailJetSender.java
@@ -1,8 +1,14 @@
 package com.kanetik.feedback.network;
 
 import android.content.Context;
+import android.text.TextUtils;
+import android.util.Base64;
 
+import com.kanetik.feedback.KanetikFeedback;
+import com.kanetik.feedback.R;
 import com.kanetik.feedback.model.Feedback;
+import com.kanetik.feedback.utility.FeedbackUtils;
+import com.kanetik.feedback.utility.LogUtils;
 import com.mailjet.client.ClientOptions;
 import com.mailjet.client.MailjetClient;
 import com.mailjet.client.MailjetRequest;
@@ -14,7 +20,12 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.lang.ref.WeakReference;
+import java.util.Locale;
+import java.util.UUID;
 
 class MailJetSender implements Sender {
     private final WeakReference<Context> context;
@@ -28,121 +39,90 @@ class MailJetSender implements Sender {
         if (context == null) return false;
 
         ClientOptions options = new ClientOptions("v3.1");
-        options.setTimeout(8000);
+        options.setTimeout(10000);
 
         MailjetClient client = new MailjetClient("2b65a83e271971453abd6d80e38d5691", "9c099f92dfbd4e33da387eef3c809494", options);
 
-        client.setDebug(MailjetClient.VERBOSE_DEBUG);
-
-        MailjetRequest request = null;
-        try {
-            request = new MailjetRequest(Emailv31.resource)
-                    .property(Emailv31.MESSAGES, new JSONArray()
-                            .put(new JSONObject()
-                                    .put(Emailv31.Message.FROM, new JSONObject()
-                                            .put("Email", "info@kanetik.com")
-                                            .put("Name", "Customer"))
-                                    .put(Emailv31.Message.TO, new JSONArray()
-                                            .put(new JSONObject()
-                                                    .put("Email", "jkane001@gmail.com")
-                                                    .put("Name", "Me")))
-                                    .put(Emailv31.Message.SUBJECT, "My first Mailjet Email!")
-                                    .put(Emailv31.Message.TEXTPART, "Greetings from Mailjet!")
-                                    .put(Emailv31.Message.HTMLPART, "<h3>Dear passenger 1, welcome to <a href=\"https://www.mailjet.com/\">Mailjet</a>!</h3><br />May the delivery force be with you!")));
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        try {
-            client.post(request);
-        } catch (MailjetException | MailjetSocketTimeoutException e) {
-            e.printStackTrace();
-        }
-
         // TODO: Get key & secret from init
-//        MailjetClient client = new MailjetClient("2b65a83e271971453abd6d80e38d5691", "9c099f92dfbd4e33da387eef3c809494", new ClientOptions("v3.1"));
-//
-//        final String developerEmail = "jkane001@gmail.com"; // TODO: Get from init
-//        final String developerName = FeedbackUtils.getAppLabel(context) + " Developer"; // TODO: Get from init
-//
-//        final String appSupportEmail = "info@kanetik.com"; // TODO: Get from init
-//        final String appSupportName = FeedbackUtils.getAppLabel(context) + " User"; // TODO: Get from init
-//
-//        final String userEmail = feedback.getFrom();
-//
-//        final String subject = String.format(Locale.getDefault(), context.getString(R.string.kanetik_feedback_email_subject), FeedbackUtils.getAppLabel(context), UUID.randomUUID().toString());
-//
-//        String plainTextEmail = feedback.getComment();
-//
-//        try {
-//            JSONObject message = new JSONObject();
-//
-//            JSONObject from = new JSONObject()
-//                    .put(Emailv31.Message.EMAIL, appSupportEmail)
-//                    .put(Emailv31.Message.NAME, appSupportName);
-//
-//            JSONObject replyTo = new JSONObject()
-//                    .put(Emailv31.Message.EMAIL, userEmail)
-//                    .put(Emailv31.Message.NAME, userEmail);
-//
-//            JSONArray to = new JSONArray()
-//                    .put(new JSONObject()
-//                            .put(Emailv31.Message.EMAIL, developerEmail)
-//                            .put(Emailv31.Message.NAME, developerName));
-//
-//            JSONArray attachments = new JSONArray();
-//
-//            File path = context.getFilesDir();
-//            File list[] = path.listFiles((dir, name) -> name.equalsIgnoreCase("application.log"));
-//            if (list != null && list.length == 1) {
-//                File log = list[0];
-//                int size = (int) log.length();
-//                byte[] data = new byte[size];
-//                try {
-//                    BufferedInputStream buf = new BufferedInputStream(new FileInputStream(log));
-//                    buf.read(data, 0, data.length);
-//                    buf.close();
-//
-//                    JSONObject logData = new JSONObject()
-//                            .put("ContentType", "text/plain")
-//                            .put("Filename", "logcat.txt")
-//                            .put("Base64Content", Base64.encodeToString(data, Base64.DEFAULT));
-//
-//                    attachments.put(logData);
-//                } catch (Exception ignored) {
-//                }
-//            }
-//
-//            plainTextEmail = TextUtils.concat(
-//                    plainTextEmail,
-//                    "\n\n\n",
-//                    feedback.getAppData().toString(),
-//                    "\n\n",
-//                    feedback.getDeviceData().toString(),
-//                    "\n\n",
-//                    feedback.getDevData().toString()).toString();
-//
-//            message.put(Emailv31.Message.FROM, from)
-//                    .put(Emailv31.Message.REPLYTO, replyTo)
-//                    .put(Emailv31.Message.TO, to)
-//                    .put(Emailv31.Message.SUBJECT, subject)
-//                    .put(Emailv31.Message.TEXTPART, "test");
-//                    //.put(Emailv31.Message.ATTACHMENTS, attachments);
-//
-//            JSONArray messageArray = new JSONArray();
-//            messageArray.put(message);
-//
-//            String finalJson = messageArray.toString(4).replace("\\", "");
-//
-//            if (KanetikFeedback.Companion.isDebug()) {
-//                LogUtils.i("POST KanetikFeedback - " + finalJson);
-//            }
-//
-//            client.setDebug(MailjetClient.VERBOSE_DEBUG);
-//            //client.setDebug(KanetikFeedback.Companion.isDebug() ? MailjetClient.VERBOSE_DEBUG : MailjetClient.NO_DEBUG);
-//            client.post(new MailjetRequest(Emailv31.resource).property(Emailv31.MESSAGES, messageArray));
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+        final String developerEmail = "jkane001@gmail.com"; // TODO: Get from init
+        final String developerName = FeedbackUtils.getAppLabel(context) + " Developer"; // TODO: Get from init
+
+        final String appSupportEmail = "info@kanetik.com"; // TODO: Get from init
+        final String appSupportName = FeedbackUtils.getAppLabel(context) + " User"; // TODO: Get from init
+
+        final String userEmail = feedback.getFrom();
+
+        final String subject = String.format(Locale.getDefault(), context.getString(R.string.kanetik_feedback_email_subject), FeedbackUtils.getAppLabel(context));
+
+        String plainTextEmail = feedback.getComment();
+
+        try {
+            JSONObject message = new JSONObject();
+
+            JSONObject from = new JSONObject()
+                    .put(Emailv31.Message.EMAIL, appSupportEmail)
+                    .put(Emailv31.Message.NAME, appSupportName);
+
+            JSONObject replyTo = new JSONObject()
+                    .put(Emailv31.Message.EMAIL, userEmail)
+                    .put(Emailv31.Message.NAME, userEmail);
+
+            JSONArray to = new JSONArray()
+                    .put(new JSONObject()
+                            .put(Emailv31.Message.EMAIL, developerEmail)
+                            .put(Emailv31.Message.NAME, developerName));
+
+            JSONArray attachments = new JSONArray();
+
+            File path = context.getFilesDir();
+            File[] list = path.listFiles((dir, name) -> name.equalsIgnoreCase("application.log"));
+            if (list != null && list.length == 1) {
+                File log = list[0];
+                int size = (int) log.length();
+                byte[] data = new byte[size];
+
+                BufferedInputStream buf = new BufferedInputStream(new FileInputStream(log));
+                buf.read(data, 0, data.length);
+                buf.close();
+
+                JSONObject logData = new JSONObject()
+                        .put("ContentType", "text/plain")
+                        .put("Filename", "logcat.txt")
+                        .put("Base64Content", Base64.encodeToString(data, Base64.DEFAULT));
+
+                attachments.put(logData);
+            }
+
+            plainTextEmail = TextUtils.concat(
+                    plainTextEmail,
+                    "\n\n\n",
+                    feedback.getAppData().toString(),
+                    "\n\n",
+                    feedback.getDeviceData().toString(),
+                    "\n\n",
+                    feedback.getDevData().toString()).toString();
+
+            message.put(Emailv31.Message.FROM, from)
+                    .put(Emailv31.Message.REPLYTO, replyTo)
+                    .put(Emailv31.Message.TO, to)
+                    .put(Emailv31.Message.SUBJECT, subject)
+                    .put(Emailv31.Message.TEXTPART, plainTextEmail);
+            //.put(Emailv31.Message.ATTACHMENTS, attachments);
+
+            JSONArray messageArray = new JSONArray();
+            messageArray.put(message);
+
+            String finalJson = messageArray.toString(4).replace("\\", "");
+
+            if (KanetikFeedback.Companion.isDebug()) {
+                LogUtils.i("POST KanetikFeedback - " + finalJson);
+            }
+
+            client.setDebug(KanetikFeedback.Companion.isDebug() ? MailjetClient.VERBOSE_DEBUG : MailjetClient.NO_DEBUG);
+            client.post(new MailjetRequest(Emailv31.resource).property(Emailv31.MESSAGES, messageArray));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         return true;
     }
